0a51fb1adddc1cfa4e73c31da446d9da490b0cc9
zangarzaynesh <zorgee012094@gmail.com>
2022-03-08 18:38:15 +0600

    db

--- cmd/forum/main.go
+++ cmd/forum/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
-	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/ZangarZaynesh/forum/db"
 	user3 "github.com/ZangarZaynesh/forum/internal/adapters/handlers/user"
 	"github.com/ZangarZaynesh/forum/internal/adapters/repository/user"
 	user2 "github.com/ZangarZaynesh/forum/internal/domain/user"
@@ -12,7 +14,7 @@ import (
 
 func main() {
 	ctx := context.Background()
-	var db *sql.DB
+	db := db.CheckDB()
 
 	router := http.NewServeMux()
 
@@ -20,4 +22,10 @@ func main() {
 	userService := user2.NewService(userRepository)
 	userHandler := user3.NewHandler(ctx, userService)
 	userHandler.Register(router)
+	fmt.Println("localhost:8080 listening...")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Printf("error lissen and serve main func: ---> %v\n", err)
+	}
+	defer db.Close()
 }
